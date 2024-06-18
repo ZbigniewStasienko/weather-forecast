02fcddbf4710128bc29199b726b3e517f8916a74
Zbigniew Stasienko <zbyszekstasienko@gmail.com>
2024-06-18 23:14:37 +0200

    deleting class so the json result looks prettier

--- src/main/java/com/stasienko/weather_forecast/model/ForecastForNext3DaysForCity.java
+++ src/main/java/com/stasienko/weather_forecast/model/ForecastForNext3DaysForCity.java
@@ -2,24 +2,43 @@ package com.stasienko.weather_forecast.model;
 
 public class ForecastForNext3DaysForCity {
 
-    private ForecastForNext3Days forecastForNext3Days;
-
     private String cityName;
+    private ForecastForADay day1;
+    private ForecastForADay day2;
+    private ForecastForADay day3;
 
-    public ForecastForNext3DaysForCity(ForecastForNext3Days forecastForNext3Days, String cityName) {
-        this.forecastForNext3Days = forecastForNext3Days;
+    public ForecastForNext3DaysForCity(ForecastForADay day1, ForecastForADay day2, ForecastForADay day3, String cityName) {
         this.cityName = cityName;
+        this.day1 = day1;
+        this.day2 = day2;
+        this.day3 = day3;
     }
 
     public ForecastForNext3DaysForCity() {
     }
 
-    public ForecastForNext3Days getForecastForNext3Days() {
-        return forecastForNext3Days;
+    public ForecastForADay getDay1() {
+        return day1;
     }
 
-    public void setForecastForNext3Days(ForecastForNext3Days forecastForNext3Days) {
-        this.forecastForNext3Days = forecastForNext3Days;
+    public void setDay1(ForecastForADay day1) {
+        this.day1 = day1;
+    }
+
+    public ForecastForADay getDay2() {
+        return day2;
+    }
+
+    public void setDay2(ForecastForADay day2) {
+        this.day2 = day2;
+    }
+
+    public ForecastForADay getDay3() {
+        return day3;
+    }
+
+    public void setDay3(ForecastForADay day3) {
+        this.day3 = day3;
     }
 
     public String getCityName() {
